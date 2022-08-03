21436a26a19242d39c114e7c25ea36d63df9f0b8
lena06051981be <lena06051981be@gmail.com>
2022-08-03 16:51:57 +0300

    .

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict"; // Строгий режим
+
+console.log("JavaScript is awesome".length);
+
+let username;
+console.log(typeof username);
+
+// const message = "JavaScript is awesome!";
+// alert(message);
+
+// Просим клиента подтвердить бронь на отель
+// и сохраняем в переменную результат работы confirm
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const value = prompt("Please enter a number!");
+// console.log(typeof value); // "string"
+// console.log(value); // "5"
